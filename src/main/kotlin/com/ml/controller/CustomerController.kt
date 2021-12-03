90e3d74a0a261fd98bee216c6e4ff0dbbefa5fa7
Ramon Pacheco <rmpacheco2@gmail.com>
2021-12-02 23:09:05 -0300

    add: request mapping

--- src/main/kotlin/com/ml/controller/CustomerController.kt
+++ src/main/kotlin/com/ml/controller/CustomerController.kt
@@ -1,9 +1,11 @@
 package com.ml.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/customers")
 class CustomerController {
     @GetMapping
     fun helloWorld(): String {
