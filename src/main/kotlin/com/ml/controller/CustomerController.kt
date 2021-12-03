b30ecbd6ec25cdf5cd118d860cc54bde599201de
Ramon Pacheco <rmpacheco2@gmail.com>
2021-12-03 20:42:33 -0300

    add: customer model

--- src/main/kotlin/com/ml/controller/CustomerController.kt
+++ src/main/kotlin/com/ml/controller/CustomerController.kt
@@ -1,5 +1,6 @@
 package com.ml.controller
 
+import com.ml.model.Customer
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/customers")
 class CustomerController {
     @GetMapping
-    fun helloWorld(): String {
-        return "Hello World!"
+    fun customer(): Customer {
+        return Customer("123", "José", "jose@teste.com");
     }
 }
---
+++ src/main/kotlin/com/ml/model/Customer.kt
@@ -0,0 +1,7 @@
+package com.ml.model
+
+data class Customer(
+    var id: String,
+    var name: String,
+    var email: String
+)
