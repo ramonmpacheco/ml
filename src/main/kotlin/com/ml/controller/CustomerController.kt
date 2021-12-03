9ae476cf003cc3ffe185434cd7e5c084452f2d69
Ramon Pacheco <rmpacheco2@gmail.com>
2021-12-02 22:09:12 -0300

    add: customer controller

---
+++ src/main/kotlin/com/ml/controller/CustomerController.kt
@@ -0,0 +1,12 @@
+package com.ml.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class CustomerController {
+    @GetMapping
+    fun helloWorld(): String {
+        return "Hello World!"
+    }
+}
