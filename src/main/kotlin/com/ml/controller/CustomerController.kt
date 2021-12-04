10f23c85e8c79233d95486673ad0ba67c99fbad7
Ramon Pacheco <rmpacheco2@gmail.com>
2021-12-04 13:40:18 -0300

    refact customer controller

--- src/main/kotlin/com/ml/controller/CustomerController.kt
+++ src/main/kotlin/com/ml/controller/CustomerController.kt
@@ -2,18 +2,24 @@ package com.ml.controller
 
 import com.ml.controller.request.PostCustomerRequest
 import com.ml.model.Customer
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @RestController
 @RequestMapping("/customers")
 class CustomerController {
+    var customers = mutableSetOf<Customer>();
+
     @GetMapping
-    fun customer(): Customer {
-        return Customer("123", "José", "jose@teste.com");
+    fun customer(): MutableSet<Customer> {
+        return customers
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     fun createCustomer(@RequestBody customer: PostCustomerRequest) {
-        println(customer);
+        customers.add(Customer(UUID.randomUUID().toString(), customer.name, customer.email))
+        println(customer)
     }
 }
