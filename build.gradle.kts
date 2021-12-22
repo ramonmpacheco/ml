0f86a3ef07af7ca19b963e2619e6ea56ff58972f
Ramon Pacheco <rmpacheco2@gmail.com>
2021-12-22 14:33:37 -0300

    add: flyway

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,9 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
+	implementation("org.flywaydb:flyway-core:8.2.3")
+	implementation("org.flywaydb:flyway-mysql:8.2.3")
+
 	runtimeOnly("mysql:mysql-connector-java")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
