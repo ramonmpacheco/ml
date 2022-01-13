3d8582d8055c9b0629728a2953198997c8622dd9
Ramon Pacheco <rmpacheco2@gmail.com>
2022-01-13 18:40:13 -0300

    add: constructor to Book

--- src/main/kotlin/com/ml/model/Book.kt
+++ src/main/kotlin/com/ml/model/Book.kt
@@ -1,6 +1,9 @@
 package com.ml.model
 
 import com.ml.enums.BookStatus
+import com.ml.enums.BookStatus.CANCELADO
+import com.ml.enums.BookStatus.DELETADO
+import java.lang.RuntimeException
 import java.math.BigDecimal
 import javax.persistence.*
 
@@ -13,10 +16,28 @@ data class Book(
     var name: String,
     @Column
     var price: BigDecimal,
-    @Column
-    @Enumerated(EnumType.STRING)
-    var status: BookStatus? = null,
     @ManyToOne
     @JoinColumn(name = "customer_id")
     var customer: Customer? = null
-)
+) {
+    @Column
+    @Enumerated(EnumType.STRING)
+    var status: BookStatus? = null
+        set(value) {
+            if (field == CANCELADO || field == DELETADO) {
+                throw RuntimeException("Status $field cannot be changed to $value")
+            }
+
+            field = value
+        }
+
+    constructor(
+        id: Int? = null,
+        name: String,
+        price: BigDecimal,
+        customer: Customer? = null,
+        status: BookStatus?
+    ) : this(id, name, price, customer) {
+        this.status = status
+    }
+}
