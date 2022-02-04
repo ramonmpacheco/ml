d7547a0388dca0344d8ab5fa7ef4a1b6c7d7467f
Ramon Pacheco <rmpacheco2@gmail.com>
2022-02-04 19:33:23 -0300

    add: async listeners

--- src/main/kotlin/com/ml/events/listeners/GenerateNfeListener.kt
+++ src/main/kotlin/com/ml/events/listeners/GenerateNfeListener.kt
@@ -3,11 +3,13 @@ package com.ml.events.listeners
 import com.ml.events.PurchaseEvent
 import com.ml.service.PurchaseService
 import org.springframework.context.event.EventListener
+import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 import java.util.*
 
 @Component
 class GenerateNfeListener(private val purchaseService: PurchaseService) {
+    @Async
     @EventListener
     fun listen(purchaseEvent: PurchaseEvent) {
        val nfe = UUID.randomUUID().toString()
--- src/main/kotlin/com/ml/events/listeners/UpdateSoldBookListener.kt
+++ src/main/kotlin/com/ml/events/listeners/UpdateSoldBookListener.kt
@@ -3,14 +3,14 @@ package com.ml.events.listeners
 import com.ml.events.PurchaseEvent
 import com.ml.service.BookService
 import org.springframework.context.event.EventListener
+import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 
 @Component
 class UpdateSoldBookListener(private val bookService: BookService) {
+    @Async
     @EventListener
     fun listen(purchaseEvent: PurchaseEvent) {
-
-
         bookService.purchase(purchaseEvent.purchase.books)
     }
 }
